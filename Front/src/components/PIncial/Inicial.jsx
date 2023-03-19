b2abb4f84540d75ffd4db548e7216e16e411c7fc
GabrielPiovesani <gabriel.piovesani@gmail.com>
2023-03-19 01:21:58 -0300

    atualizando o projeto com o front

---
+++ Front/src/components/PIncial/Inicial.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import  './Inicial.module.css'
+
+
+export default function Inicial() {
+  return (
+      <>
+          
+          <Container className='d-flex '>
+        
+              <h1>ValDisnei</h1>
+    
+          </Container>
+      </>
+  )
+}
