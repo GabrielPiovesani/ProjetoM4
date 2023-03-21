204a1186e4a25718e4fca6790b126c22ee287698
lLeoSantos <leonnarddo15@gmail.com>
2023-03-21 01:25:10 -0300

    Ajustei  uns detalhes em loguin - Coloquei o footer no final da pagina

--- Front/src/pages/Login/Login.jsx
+++ Front/src/pages/Login/Login.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import LoginMenu from '../../components/LoginMenu/LoginMenu.jsx';
 import Form from '../../components/Form/Form.jsx';
-
-
-
-
-
-
-
-
-
-
+import Footer from '../../components/Footer/Footer.jsx';
 
 const Login =()=> {
     return (
       
         <div className="Login-background">
          <LoginMenu/>
-
          <Form/>
+         <Footer></Footer>
         </div>     
 
     );
