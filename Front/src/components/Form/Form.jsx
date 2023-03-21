fe10aaa7f4e6935084faa6b2c5f58a9ccddfb8cd
Eloisasilva98 <eloisa.1998.silva@gmail.com>
2023-03-21 01:25:34 -0300

    implementei validação na pagina login

--- Front/src/components/Form/Form.jsx
+++ Front/src/components/Form/Form.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from '../LoginMenu/Login.module.css';
+import {validaEmail} from './FormValidacao';
 
 const Form = () => {
+
+
+  const [emailErr, setEmailErr] = useState (false)
+  const [email,setEmail] = useState ("")
+
+  const FormValidacao = () =>{
+    if(!validaEmail.test(email)){
+      setEmailErr(true)
+    }else{
+      setEmailErr(false)
+    }
+  }
   
   return(
   
- <div className="d-flex justify-content-center align-items-center">
+ <div className="d-flex justify-content-center">
       <form className="containerLogin">
     <div class = "form-group">
         <h2>Entrar</h2>
       <label for="exampleInputEmail1">Email</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email"></input>
+      <input value={email} onChange={e => setEmail(e.target.value)}type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email"></input>
+      {emailErr && <p>Por favor, digite um email valido!</p>} 
     </div>
     <div class="form-group">
       <label>Senha</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="senha"></input>
+      <input  type="password" class="form-control" id="exampleInputPassword1" placeholder="senha"></input>
     </div>
     <div className={style.buttonLogin}>
-<button  type="submit" class="btn btn-info">Enviar</button>
+<button  type="submit" class="btn btn-success"onClick={FormValidacao}>
+  Enviar </button>
 <p>Não tem ValDisnei? Assine</p>
 </div>
       </form>
